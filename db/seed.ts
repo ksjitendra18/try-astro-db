0410c4399a7c05551f941adb98cfa837d3940c72
ksjitendra18 <jitendraks1803@gmail.com>
2024-03-19 21:15:37 +0530

    REMOVED seed script

--- db/seed.ts
+++ db/seed.ts
@@ -1,15 +1,4 @@
-import { db, users } from "astro:db";
-import { faker } from "@faker-js/faker";
 // https://astro.build/db/seed
 export default async function seed() {
   // TODO
-
-  for (let i = 0; i < 500; i++) {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    await db.insert(users).values({
-      name: `${firstName} ${lastName}`,
-      email: faker.internet.email({ firstName, lastName }),
-    });
-  }
 }
